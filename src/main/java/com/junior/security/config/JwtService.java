b473581a8e459027c347f368e1110552e65f96ee
Cleonildo Jr <cleu.junior@gmail.com>
2023-01-05 11:33:00 -0300

    Setting JWT

---
+++ src/main/java/com/junior/security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.junior.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return "";
+    }
+}
