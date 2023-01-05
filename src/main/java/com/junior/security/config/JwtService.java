030d9fac2a650d0d36d61e72d8efc485888476ba
Cleonildo Jr <cleu.junior@gmail.com>
2023-01-05 20:52:41 -0300

    Setting Keys.

--- src/main/java/com/junior/security/config/JwtService.java
+++ src/main/java/com/junior/security/config/JwtService.java
@@ -1,11 +1,31 @@
 package com.junior.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "792F423F4528482B4D6251655368566D597133743677397A24432646294A404E";
 
     public String extractUsername(String token) {
         return "";
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
